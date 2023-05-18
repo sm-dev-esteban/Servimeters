fe8b8d7a1e05eb9c2230768872754f785ecd95ab
Esteban Serna <esteban.serna@servimeters.com>
2023-05-18 14:10:20 -0500

    cambio para emitir eventos a roles o usuario

--- assets/js/server.js
+++ assets/js/server.js
@@ -1,16 +1,33 @@
+// No sé qué tanto se pueda agrandar un archivo como este, así que mejor lo manejo comprimido.
+// https://www.toptal.com/developers/javascript-minifier
+
 const server = new WebSocket('ws://localhost:8080');
 
 server.onopen = function (e) {
-    console.log("Socket active");
+    session = automaticForm("readSession");
+
+    localStorage.setItem("rol", session.rol);
+    localStorage.setItem("usuario", session.usuario);
+
 }
 
 server.onmessage = function (e) {
     resp = JSON.parse(e.data);
-
+    // Enviar resp.type con la acción que quieran realizar y diviertanse :).
     if (resp.type == "alert") {
         arrayAlert = resp.data.arrayAlert ? resp.data.arrayAlert : {};
         typeAlert = resp.data.typeAlert ? resp.data.typeAlert : "Sweetalert2";
-        alerts(arrayAlert, typeAlert);
+
+        rol = String(localStorage.getItem("rol"));
+        usuario = String(localStorage.getItem("usuario"));
+        
+        if (resp.rol == rol) {
+            alerts(arrayAlert, typeAlert);
+        } else if (resp.usuario == usuario) {
+            alerts(arrayAlert, typeAlert);
+        } else if (!resp.rol && !resp.usuario) {
+            alerts(arrayAlert, typeAlert);
+        }
     }
 
-};
+}
