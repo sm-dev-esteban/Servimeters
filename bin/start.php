45dfd3a89dcea3562747fe3df36651a5f6a2c2f3
Esteban Serna <esteban.serna@servimeters.com>
2023-05-30 10:19:42 -0500

    El comando se me ejecuto una vez dos veces y no sé por qué, así que mejor hice que el archivo me creara un archivo .bat para que valide primero si el puerto está activo y hay si ejecute el comando.

--- bin/start.php
+++ bin/start.php
@@ -4,12 +4,39 @@ require("../controller/automaticForm.php");
 
 $config = AutomaticForm::getConfig();
 
+$cmd = 'cd ' . $config->FOLDER_SITE . ' & composer update & php ' . __DIR__ . '\server.php';
+
+$check_bat = fopen("check.bat", "w");
+fwrite($check_bat, '
+@echo off
+
+setlocal
+
+REM Puerto a verificar
+set "puerto=8080"
+
+REM Comando a ejecutar si encuentra el puerto
+set "comando=' . $cmd . '"
+
+REM Verificar el puerto
+netstat -ano | findstr ":%puerto%" >nul
+
+REM Si encuentra el puerto, ejecuta el comando
+if %errorlevel% equ 0 (
+    echo El puerto %puerto% esta activo.
+) else (
+    %comando%
+)
+
+endlocal
+');
+fclose($check_bat);
+
 // comandos
 // 1. Entra a la carpeta del proyecto
 // 2. Actualiza composer
 // 3. Ejecuta el servidor
-$cmd = 'cd ' . $config->FOLDER_SITE . ' & composer update & php ' . __DIR__ . '\server.php';
-executeCMD('start cmd.exe @cmd /k "' . $cmd . '"'); 
+executeCMD('start cmd.exe @cmd /k "' . __DIR__ . '\check.bat"');
 
 function executeCMD($cmd)
 {
