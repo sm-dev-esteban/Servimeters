d1ed097d97a950f366b0ad81ed85d8e4247bca5f
Esteban Serna <esteban.serna@servimeters.com>
2023-06-13 11:48:55 -0500

    Corrección a la función que accede a los métodos estáticos de una clase. Realmente estoy probando si tengo git que se venció el token de acceso :n

--- controller/af.controller.php
+++ controller/af.controller.php
@@ -7,35 +7,16 @@ require_once('automaticForm.php');
 // $_GET = json_decode(file_get_contents('php://input'), true);
 
 $action = isset($_GET["action"]) ? $_GET["action"] : "";
-$params = isset($_POST["param"]) ? $_POST["param"] : "";
+$params = isset($_POST["param"]) && is_array($_POST["param"]) ? $_POST["param"] : [""];
 
 define("ACTION", $action);
 
 if (array_filter(AutomaticForm::getClassMethods(), function ($x) {
     return $x["name"] == ACTION;
 })) {
-    // echo json_encode(AutomaticForm::$action(implode(', ', array_map(function ($x) {
-    //     $r = random();
-    //     $val[$r] = $x;
-    //     return $val[$r];
-    // }, $params))), JSON_UNESCAPED_UNICODE);
-    // exit();
-    $i = -1;
-    echo json_encode(AutomaticForm::$action(
-        isset($params[$i += 1]) ? $params[$i] : "",
-        isset($params[$i += 1]) ? $params[$i] : "",
-        isset($params[$i += 1]) ? $params[$i] : "",
-        isset($params[$i += 1]) ? $params[$i] : "",
-        isset($params[$i += 1]) ? $params[$i] : "",
-        isset($params[$i += 1]) ? $params[$i] : ""
-    ), JSON_UNESCAPED_UNICODE);
+    echo json_encode(AutomaticForm::$action(...$params));
     exit();
 } else {
     echo json_encode(["error" => "action in undefined"], JSON_UNESCAPED_UNICODE);
     exit();
 }
-
-// function ramdon()
-// {
-//     return rand(0, date("YmdHis"));
-// }
