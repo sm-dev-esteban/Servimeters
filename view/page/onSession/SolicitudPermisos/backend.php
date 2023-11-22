c32d78f436e128f5c5c4b139230b2337d39fd14a
Esteban Serna <esteban.serna@servimeters.com>
2023-11-22 10:29:52 -0500

    Nueva Vista

---
+++ view/page/onSession/SolicitudPermisos/backend.php
@@ -0,0 +1,8 @@
+<?php+
+/*-- 2023-11-22 10:29:13 --*/
+
+include_once "C:/xampp/htdocs/Servimeters/vendor/autoload.php";
+include "C:/xampp/htdocs/Servimeters/Config.php";
+include "C:/xampp/htdocs/Servimeters/conn.php";
+
+date_default_timezone_set(TIMEZONE);
---
+++ view/page/onSession/SolicitudPermisos/frontend.js
@@ -0,0 +1,5 @@
+/*-- 2023-11-22 10:29:13 --*/
+
+$(document).ready(async () => {
+    console.log(`pozole`)
+})
