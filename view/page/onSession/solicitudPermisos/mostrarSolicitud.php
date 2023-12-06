ba5e50cd98685367237daea1a070f1276eb789c9
Duvan Camilo Sanabria Murcia <duvan.sanabria@servimeterssa.com>
2023-12-06 17:11:35 -0500

    Implementanción listado de usuarios

---
+++ view/page/onSession/solicitudPermisos/mostrarSolicitud.php
@@ -0,0 +1,77 @@
+<div class="card-body p-0">
+  <!--Grupo de botones de cabecera de tablas-->
+  <div class="mailbox-controls">
+    <button type="button" class="btn btn-default btn-sm checkbox-toggle"><i class="far fa-square"></i></button>
+    <div class="btn-group">
+      <button type="button" class="btn btn-default btn-sm"><i class="far fa-trash-alt"></i></button>
+      <button type="button" class="btn btn-default btn-sm"><i class="fas fa-reply"></i></button>
+      <button type="button" class="btn btn-default btn-sm"><i class="fas fa-share"></i></button>
+    </div>
+
+    <button type="button" class="btn btn-default btn-sm"><i class="fas fa-sync-alt"></i></button>
+    <div class="float-right">1-50/200
+      <div class="btn-group">
+        <button type="button" class="btn btn-default btn-sm"><i class="fas fa-chevron-left"></i></button>
+        <button type="button" class="btn btn-default btn-sm"><i class="fas fa-chevron-right"></i></button>
+    </div>
+    </div>
+  </div>
+  <!--Tabla de usuarios de permiso-->
+  <div class="table-responsive mailbox-messages">
+    <table class="table table-hover table-striped projects">
+      <thead class="bg-blue">
+        <tr>
+          <th style="width: 1%">Código</th>
+          <th style="width: 5%">Funcionario</th>             
+          <th style="width: 5%">Cédula</th>
+          <th style="width: 5%">Dependencia</th>
+          <th style="width: 10%">Fecha Inicio-Fin</th>
+          <th style="width: 5%">No. Horas</th>
+          <th style="width: 5%">T. Permiso</th>
+          <th style="width: 5%">Jefe</th>
+          <th style="width: 5%">Reposición</th>
+          <th style="width: 5%">Aprobado</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>13232</td>
+          <td>Hola Mundo</td>
+          <td>1212346454</td>
+          <td>Técnologia y desarrollo</td>
+          <td>12/06/23 - 13/01/23</td>
+          <td>15</td>
+          <td>Cita Médica</td>
+          <td>Gloria </td>
+          <td>Sí </td>
+          <td>Aprobado</td>
+
+        </tr>
+        <tr>
+          <td>13232</td>
+          <td>Hola Mundo</td>
+          <td>1212346454</td>
+          <td>Técnologia y desarrollo</td>
+          <td>12/06/23 - 13/01/23</td>
+          <td>15</td>
+          <td>Cita Médica</td>
+          <td>Gloria </td>
+          <td>Sí </td>
+          <td>Aprobado</td>
+        </tr>
+        <tr>
+          <td>13232</td>
+          <td>Hola Mundo</td>
+          <td>1212346454</td>
+          <td>Técnologia y desarrollo</td>
+          <td>12/06/23 - 13/01/23</td>
+          <td>15</td>
+          <td>Cita Médica</td>
+          <td>Gloria </td>
+          <td>Sí </td>
+          <td>Aprobado</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</div>
