fbcc7d41de93fd0eaef57da8fe598be5a22e1446
Duvan Camilo Sanabria Murcia <duvan.sanabria@servimeterssa.com>
2023-12-11 15:54:34 -0500

    Agregación filtro en la opción mostrar solicitud

--- view/page/onSession/solicitudPermisos/mostrarSolicitud.php
+++ view/page/onSession/solicitudPermisos/mostrarSolicitud.php
@@ -8,6 +8,61 @@
       <button type="button" class="btn btn-default btn-sm"><i class="fas fa-share"></i></button>
     </div>
 
+    <!--Filtro por código-->
+    <div class="btn-group">
+      <div class="select2-blue">
+        <select class="select2" multiple="multiple" data-placeholder="Filtro por código" data-dropdown-css-class="select2-blue" style="width: 100%;">
+          <option>453543</option>
+          <option>4654</option>
+          <option>45654</option>
+        </select>
+      </div>
+    </div>
+
+      <!--Filtro por funcionario-->
+      <div class="btn-group">
+      <div class="select2-blue">
+        <select class="select2" multiple="multiple" data-placeholder="Filtro por funcionario" data-dropdown-css-class="select2-blue" style="width: 100%;">
+          <option>Esteban</option>
+          <option>Camilo</option>
+          <option>Gladis</option>
+        </select>
+      </div>
+    </div>
+  
+    <!--Filtro por área-->
+    <div class="btn-group">
+      <div class="select2-blue">
+        <select class="select2" multiple="multiple" data-placeholder="Filtro por área" data-dropdown-css-class="select2-blue" style="width: 100%;">
+          <option>Técnologia</option>
+          <option>Comercial</option>
+          <option>Retie</option>
+        </select>
+      </div>
+
+      <!--Filtro por funcionario-->
+      <div class="btn-group">
+        <div class="select2-blue">
+          <select class="select2" multiple="multiple" data-placeholder="Filtro por jefe" data-dropdown-css-class="select2-blue" style="width: 100%;">
+            <option>Esteban</option>
+            <option>Camilo</option>
+            <option>Gladis</option>
+          </select>
+        </div>
+      </div>
+    </div>
+
+      <!--Filtro por tipo de permiso-->
+      <div class="btn-group">
+        <div class="select2-blue">
+          <select class="select2" multiple="multiple" data-placeholder="Filtro por tipo de permiso" data-dropdown-css-class="select2-blue" style="width: 100%;">
+            <option>Médico</option>
+            <option>Cumpleaños</option>
+            <option>Otro</option>
+          </select>
+        </div>
+      </div>
+
     <button type="button" class="btn btn-default btn-sm"><i class="fas fa-sync-alt"></i></button>
     <div class="float-right">1-50/200
       <div class="btn-group">
