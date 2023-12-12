0a231b1029950ea3116c10381dd20b8c446dc857
Esteban Serna <esteban.serna@servimeters.com>
2023-12-12 07:37:23 -0500

    Vista de solicitudes

--- view/page/onSession/solicitudPermisos/mostrarSolicitud.php
+++ view/page/onSession/solicitudPermisos/mostrarSolicitud.php
@@ -1,132 +1,73 @@
-<div class="card-body p-0">
-  <!--Grupo de botones de cabecera de tablas-->
-  <div class="mailbox-controls">
-    <button type="button" class="btn btn-default btn-sm checkbox-toggle"><i class="far fa-square"></i></button>
-    <div class="btn-group">
-      <button type="button" class="btn btn-default btn-sm"><i class="far fa-trash-alt"></i></button>
-      <button type="button" class="btn btn-default btn-sm"><i class="fas fa-reply"></i></button>
-      <button type="button" class="btn btn-default btn-sm"><i class="fas fa-share"></i></button>
-    </div>
+<?php
 
-    <!--Filtro por código-->
-    <div class="btn-group">
-      <div class="select2-blue">
-        <select class="select2" multiple="multiple" data-placeholder="Filtro por código" data-dropdown-css-class="select2-blue" style="width: 100%;">
-          <option>453543</option>
-          <option>4654</option>
-          <option>45654</option>
-        </select>
+use Model\RouteModel;
+
+$routeM = new RouteModel;
+?>
+
+<section class="content-header">
+  <div class="container-fluid">
+    <div class="row mb-2">
+      <div class="col-sm-6">
+        <h1>Mostrar solicitud</h1>
+      </div>
+      <div class="col-sm-6">
+        <ol class="breadcrumb float-sm-right">
+          <li class="breadcrumb-item active"><?= substr($routeM->getURI(), 1) ?></li>
+        </ol>
       </div>
     </div>
+  </div>
+</section>
+<section class="content">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-sm-12">
+        <div class="card card-primary card-outline mt-1">
+          <div class="card-header">
+            <h3 class="card-title">Solicitudes</h3>
+            <div class="card-tools">
+              <div class="input-group input-group-sm">
+                <input type="text" class="form-control" placeholder="Buscar">
+              </div>
+            </div>
+          </div>
 
-      <!--Filtro por funcionario-->
-      <div class="btn-group">
-      <div class="select2-blue">
-        <select class="select2" multiple="multiple" data-placeholder="Filtro por funcionario" data-dropdown-css-class="select2-blue" style="width: 100%;">
-          <option>Esteban</option>
-          <option>Camilo</option>
-          <option>Gladis</option>
-        </select>
-      </div>
-    </div>
-  
-    <!--Filtro por área-->
-    <div class="btn-group">
-      <div class="select2-blue">
-        <select class="select2" multiple="multiple" data-placeholder="Filtro por área" data-dropdown-css-class="select2-blue" style="width: 100%;">
-          <option>Técnologia</option>
-          <option>Comercial</option>
-          <option>Retie</option>
-        </select>
-      </div>
-
-      <!--Filtro por funcionario-->
-      <div class="btn-group">
-        <div class="select2-blue">
-          <select class="select2" multiple="multiple" data-placeholder="Filtro por jefe" data-dropdown-css-class="select2-blue" style="width: 100%;">
-            <option>Esteban</option>
-            <option>Camilo</option>
-            <option>Gladis</option>
-          </select>
+          <div class="card-body p-0">
+            <!-- Agrega DataTables a la tabla existente -->
+            <table data-action="ssp_Solicitud" class="table table-hover table-striped">
+              <thead class="bg-blue">
+                <tr>
+                  <th>id</th>
+                  <th>funcionario</th>
+                  <th>fecha registro</th>
+                  <th>tipo de permiso</th>
+                  <th>fecha Inicio - Fin</th>
+                  <th>Horas</th>
+                  <th>observacion</th>
+                  <th>reposicion</th>
+                  <th>fecha reposicion</th>
+                  <th>aprovador</th>
+                </tr>
+              </thead>
+              <tfoot>
+                <tr>
+                  <th><input type="text" class="form-control" placeholder="id"></th>
+                  <th><input type="text" class="form-control" placeholder="funcionario"></th>
+                  <th><input type="text" class="form-control" placeholder="fechaRegistro"></th>
+                  <th><input type="text" class="form-control" placeholder="tipoPermiso"></th>
+                  <th><input type="text" class="form-control" placeholder="fechaInicioFin"></th>
+                  <th><input type="text" class="form-control" placeholder="Nhoras"></th>
+                  <th><input type="text" class="form-control" placeholder="observacion"></th>
+                  <th><input type="text" class="form-control" placeholder="reposicion"></th>
+                  <th><input type="text" class="form-control" placeholder="fechaReposicion"></th>
+                  <th><input type="text" class="form-control" placeholder="aprovador"></th>
+                </tr>
+              </tfoot>
+            </table>
+          </div>
         </div>
       </div>
     </div>
-
-      <!--Filtro por tipo de permiso-->
-      <div class="btn-group">
-        <div class="select2-blue">
-          <select class="select2" multiple="multiple" data-placeholder="Filtro por tipo de permiso" data-dropdown-css-class="select2-blue" style="width: 100%;">
-            <option>Médico</option>
-            <option>Cumpleaños</option>
-            <option>Otro</option>
-          </select>
-        </div>
-      </div>
-
-    <button type="button" class="btn btn-default btn-sm"><i class="fas fa-sync-alt"></i></button>
-    <div class="float-right">1-50/200
-      <div class="btn-group">
-        <button type="button" class="btn btn-default btn-sm"><i class="fas fa-chevron-left"></i></button>
-        <button type="button" class="btn btn-default btn-sm"><i class="fas fa-chevron-right"></i></button>
-    </div>
-    </div>
   </div>
-  <!--Tabla de usuarios de permiso-->
-  <div class="table-responsive mailbox-messages">
-    <table class="table table-hover table-striped projects">
-      <thead class="bg-blue">
-        <tr>
-          <th style="width: 1%">Código</th>
-          <th style="width: 5%">Funcionario</th>             
-          <th style="width: 5%">Cédula</th>
-          <th style="width: 5%">Dependencia</th>
-          <th style="width: 10%">Fecha Inicio-Fin</th>
-          <th style="width: 5%">No. Horas</th>
-          <th style="width: 5%">T. Permiso</th>
-          <th style="width: 5%">Jefe</th>
-          <th style="width: 5%">Reposición</th>
-          <th style="width: 5%">Aprobado</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>13232</td>
-          <td>Hola Mundo</td>
-          <td>1212346454</td>
-          <td>Técnologia y desarrollo</td>
-          <td>12/06/23 - 13/01/23</td>
-          <td>15</td>
-          <td>Cita Médica</td>
-          <td>Gloria </td>
-          <td>Sí </td>
-          <td>Aprobado</td>
-
-        </tr>
-        <tr>
-          <td>13232</td>
-          <td>Hola Mundo</td>
-          <td>1212346454</td>
-          <td>Técnologia y desarrollo</td>
-          <td>12/06/23 - 13/01/23</td>
-          <td>15</td>
-          <td>Cita Médica</td>
-          <td>Gloria </td>
-          <td>Sí </td>
-          <td>Aprobado</td>
-        </tr>
-        <tr>
-          <td>13232</td>
-          <td>Hola Mundo</td>
-          <td>1212346454</td>
-          <td>Técnologia y desarrollo</td>
-          <td>12/06/23 - 13/01/23</td>
-          <td>15</td>
-          <td>Cita Médica</td>
-          <td>Gloria </td>
-          <td>Sí </td>
-          <td>Aprobado</td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
-</div>
+</section>
